99e9aec46fc7501de000e9c576410bb202c8fb52
fianznieh2010 <fianznieh@gmail.com>
2024-07-20 03:26:22 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-global.owner = ["62887435047326"]
-global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
+global.owner = ["6283183361903"]
+global.push_kontak_delay = 7000 //7 detik, 1000 = 1 detik
 global.pairingNumber = ""
